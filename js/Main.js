5e8ed9f90b69e75cefcbf4fe1519d9bcd6b32568
Lucas Rocca <lucas.98.rocca@gmail.com>
2023-03-24 18:35:16 -0300

    Version 2.0

---
+++ js/Main.js
@@ -0,0 +1,7 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show')
+});
+/* agregar scroll (efecto) para que vayan apareciendo los div
+ScrollReveal().reveal('.asdasd');
+ScrollReveal().reveal('.asdasd' , {delay: 500});
+ScrollReveal().reveal('.asdasd' , {delay: 500});*/
