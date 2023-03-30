0c77ebb7d893abb3c99b24f741e1129a04141b0e
lucas <lucas.98.rocca@gmail.com>
2023-03-30 05:08:01 -0300

    se agrega btn wpp

--- js/Main.js
+++ js/Main.js
@@ -1,7 +1,4 @@
 document.querySelector('.menu-btn').addEventListener('click', () => {
     document.querySelector('.nav-menu').classList.toggle('show')
 });
-/* agregar scroll (efecto) para que vayan apareciendo los div
-ScrollReveal().reveal('.asdasd');
-ScrollReveal().reveal('.asdasd' , {delay: 500});
-ScrollReveal().reveal('.asdasd' , {delay: 500});*/
+
